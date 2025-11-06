4ecb412af99c4c69cc91f8cf8d746d468dfdef08
Rahul Kamat <kamatrahul415@gmail.com>
2025-11-06 14:41:56 +0530

    first commit

---
+++ ReactJS/layouttask/src/App.js
@@ -0,0 +1,15 @@
+import About from "./Layout/pages/About";
+import Contact from "./Layout/pages/Contact";
+import Home from "./Layout/pages/Home";
+
+function App() {
+  return (
+    <>
+    <Home/>
+    <About/>
+    <Contact/>
+    </>
+  );
+}
+
+export default App;
