48d253e0e12b86365bfa30078478319088ea7b45
Rahul Kamat <kamatrahul415@gmail.com>
2025-11-08 14:24:46 +0530

    first commit

--- ReactJS/layouttask/src/Layout/pages/Home.jsx
+++ ReactJS/layouttask/src/Layout/pages/Home.jsx
@@ -41,6 +41,7 @@ function Home() {
             <h5>Title description, Sep 2, 2020</h5>
             <div className="fakeimg">Fake Image</div>
             <p>Some text..</p>
+            <h1 className='box1'>Hii i am External CSS</h1>
             <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
           </div>
         </div>
